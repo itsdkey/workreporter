865c3b03ce8705fcc7360c0345aef956d9bb0c90
Dawid Kosiński <tofik1432@gmail.com>
2020-10-25 11:54:25 +0100

    Resolves #21 - changelog

--- reporter/apps.py
+++ reporter/apps.py
@@ -11,7 +11,7 @@ from .conf import SLACK_CHANNEL_ID, SLACK_TOKEN
 from .slughify import slughifi
 from .utils import get_value_from_redis, set_key_in_redis
 
-__version__ = '0.0.5'
+__version__ = '0.0.6'
 
 
 class JiraApp:
